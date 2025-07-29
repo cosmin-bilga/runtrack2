ba4bd14df74c43f7afb3965a587d7a81442aef3a
cosmin-bilga <cosmin.bilga@laplateforme.io>
2025-07-29 15:20:35 +0200

    Jour 10

---
+++ jour10/job13/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour 10 - Job 13</title>
+    <style>
+        table,
+        td,
+        th {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 3px;
+        }
+    </style>
+</head>
+
+<body>
+
+
+
+    <?php
+
+    $mysqli = new mysqli("localhost", "root", "", "jour09", 3307);
+    $mysqli->set_charset("utf8");
+    $result = $mysqli->query("SELECT salle.nom AS 'Nom salle',etage.nom AS 'Nom etage' FROM salle INNER JOIN etage ON salle.id_etage=etage.id;");
+
+
+
+    ?>
+    <table>
+        <thead>
+            <tr>
+                <?php
+                $row = $result->fetch_assoc();
+                foreach ($row as $key => $value)
+                    echo "<th>" . ucfirst($key) . "</th>";
+                ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($row) {
+                echo "<tr>";
+                foreach ($row as $cell)
+                    echo "<td>$cell</td>";
+                echo "</tr>";
+                $row = $result->fetch_assoc();
+                //print_r($row);
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
